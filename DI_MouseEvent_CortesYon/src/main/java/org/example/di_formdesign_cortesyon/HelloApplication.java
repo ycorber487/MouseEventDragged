42b3c7a548e3dad5dfffd9df10458950f83d12c6
ycorber487 <163828691+ycorber487@users.noreply.github.com>
2024-10-15 18:18:35 +0200

    Update HelloApplication.java

--- DI_MouseEvent_CortesYon/src/main/java/org/example/di_formdesign_cortesyon/HelloApplication.java
+++ DI_MouseEvent_CortesYon/src/main/java/org/example/di_formdesign_cortesyon/HelloApplication.java
@@ -13,18 +13,18 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage primaryStage) {
 
-        Pane pane = new Pane();
-        Text text = new Text(5,10,"Programming is fun");
+        Pane pane = new Pane(); // Contenedor grafico que contendra los elementos
+        Text text = new Text(5,10,"Programming is fun"); // Colocamos el objeto texto en la posicion 5 10 con "Programming is fun"
         
-        text.setOnMouseDragged(e ->{
+        text.setOnMouseDragged(e ->{ // creamos un evento si dejas el raton pulsado sobre el texto
             text.setX(e.getX());
             text.setY(e.getY());
         });
 
-        pane.getChildren().add(text);
+        pane.getChildren().add(text); // agrega texto al contenedor
 
-        Scene scene = new Scene(pane, 600, 400);
-        primaryStage.setTitle("Ejemplo de arrastre");
+        Scene scene = new Scene(pane, 600, 400); // le damos tamanio a la ventana
+        primaryStage.setTitle("Ejemplo de arrastre"); // colocamos un titulo
         primaryStage.setScene(scene);
         primaryStage.show();
     }
